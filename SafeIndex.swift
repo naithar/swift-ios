9ee2e3bf119b4e7e642c713c40afcc7bf1e775b1
Naithar <devias.naith@gmail.com>
2016-06-03 20:40:31 +0300

    safe index fix

--- SafeIndex.swift
+++ SafeIndex.swift
@@ -1,13 +1,44 @@
+//  Copyright (c) 2016 Sergey Minakov
 //
-//  SafeIndex.swift
-//  
-//
-//  Created by Sergey Minakov on 03.06.16.
-//
-//
+//  Permission is hereby granted, free of charge, to any person obtaining
+//  a copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the Software
+//  is furnished to do so, subject to the following conditions:
 
-import UIKit
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
 
-class SafeIndex: UINavigationController {
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+//  OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+
+extension MutableCollectionType {
+    
+    subscript (safe index: Index) -> Generator.Element? {
+        get {
+            return self.indices.contains(index) ? self[index] : nil
+        }
+        set {
+            if let value = newValue
+                where self.indices.contains(index) {
+                self[index] = value
+            }
+        }
+    }
+}
+
+extension CollectionType {
+    
+    subscript (safe index: Index) -> Generator.Element? {
+        get {
+            return self.indices.contains(index) ? self[index] : nil
+        }
+    }
 }
